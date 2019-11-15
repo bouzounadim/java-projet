7a7c7fe5c988d2402aefd5f8a6463590a2ef0896
nadim <bouzaffaranadim@gmail.com>
2019-11-15 09:55:03 +0100

    update project

--- demo-1/src/main/java/com/example/demo/Controller/HomeController.java
+++ demo-1/src/main/java/com/example/demo/Controller/HomeController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
-
-
-	  @RequestMapping("home")
+	  @RequestMapping("/")
 	  public String home() {
 		System.out.println("nadim");
 		return "home.html";
