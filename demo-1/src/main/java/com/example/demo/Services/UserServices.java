29baa8f1a911164f1e91e65c02a5ad7730b4d213
nadim <bouzaffaranadim@gmail.com>
2019-11-15 10:23:44 +0100

    Repo and services update

---
+++ demo-1/src/main/java/com/example/demo/Services/UserServices.java
@@ -0,0 +1,11 @@
+package com.example.demo.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.Repositry.UserRepo;
+
+public class UserServices {
+	@Autowired
+	UserRepo userrepo;
+
+}
